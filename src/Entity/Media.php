9e4b877221e88d798f2ba71863efe3ec2fe6ffb9
Akzel <axelbrandau@hotmail.com>
2020-02-04 15:30:30 +0100

    media

---
+++ src/Entity/Media.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MediaRepository")
+ */
+class Media
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $AddedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAddedAt(): ?\DateTimeInterface
+    {
+        return $this->AddedAt;
+    }
+
+    public function setAddedAt(?\DateTimeInterface $AddedAt): self
+    {
+        $this->AddedAt = $AddedAt;
+
+        return $this;
+    }
+}
