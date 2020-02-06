09c1f64f6328cec5337b4df225fcfc6486c01467
Akzel <axelbrandau@hotmail.com>
2020-02-06 16:28:14 +0100

    caca

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -3,9 +3,12 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\MediaRepository")
+ * @Vich\Uploadable
  */
 class Media
 {
@@ -25,6 +28,18 @@ class Media
      * @ORM\Column(type="datetime", nullable=true)
      */
     private $AddedAt;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="media_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+
 
     public function getId(): ?int
     {
@@ -53,5 +68,34 @@ class Media
         $this->AddedAt = $AddedAt;
 
         return $this;
+    }
+
+
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
     }
 }
